6ce6f1ffaf6f4d6f04d4698ccc3a4698f617d2cc
Adam Weizman <adamweizman@gmail.com>
2020-07-24 15:47:17 -0400

    Initial Commit

---
+++ Create_Prototype_2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float fireInterval = 1.0f;
+    private float nextFire = 0.0f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = nextFire + fireInterval;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
